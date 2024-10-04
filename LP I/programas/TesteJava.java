b7f687472fd94c353dea18928c96b22284b11447
antonio809 <antoniojorgeqk@gmail.com>
2024-10-04 18:29:54 -0300

    Teste com lp

---
+++ LP I/programas/TesteJava.java
@@ -0,0 +1,9 @@
+package programas;
+import java.util.Scanner;
+
+public class TesteJava {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+}
