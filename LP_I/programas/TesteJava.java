92227d88f6944589b898c5facb33a5667da47405
antonio809 <antoniojorgeqk@gmail.com>
2024-10-04 20:18:42 -0300

    pronto

--- LP_I/programas/TesteJava.java
+++
@@ -1,9 +0,0 @@
-package programas;
-import java.util.Scanner;
-
-public class TesteJava {
-    public static void main(String[] args) {
-        
-        System.out.println("Hello World");
-    }
-}
